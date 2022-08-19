45d76060ee6948c98c805c8393ebd4bd92897bf2
Vittorio Adesso <adessovittorio8@gmail.com>
2022-08-19 18:20:40 +0200

    new features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule, 
-    PrimeNgModule
+    BrowserModule,
+    PrimeNgModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,17 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import {ButtonModule} from 'primeng/button';
 import {CardModule} from 'primeng/card';
+import {MenubarModule} from 'primeng/menubar';
+// import {MenuItem} from 'primeng/api';
 
 @NgModule({
   declarations: [],
   exports:[
     ButtonModule,
-    CardModule
+    CardModule,
+    MenubarModule, 
+    // MenuItem
   ],
   imports: [
     CommonModule,
     ButtonModule,
-    CardModule
+    CardModule,
   ]
 })
 export class PrimeNgModule { }
