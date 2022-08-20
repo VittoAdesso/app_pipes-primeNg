82fe983d3c24fb53bfa841cfd818efcafe43c271
Vittorio Adesso <adessovittorio8@gmail.com>
2022-08-20 10:20:52 +0200

    adding routes to pages & home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasicosComponent } from './ventas/pages/basicos/basicos.component';
+import { NoComunesComponent } from './ventas/pages/no-comunes/no-comunes.component';
+import { NumerosComponent } from './ventas/pages/numeros/numeros.component';
+import { OrdenarComponent } from './ventas/pages/ordenar/ordenar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    component: BasicosComponent,
+    pathMatch: 'full'
+  }, 
+  {
+    path: 'no-comunes', 
+    component: NoComunesComponent,
+  },
+  {
+    path: 'numeros', 
+    component: NumerosComponent,
+  },
+  {
+    path: 'ordenar', 
+    component: OrdenarComponent,
+  },
+  {
+    path: '**', 
+    redirectTo: '',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
