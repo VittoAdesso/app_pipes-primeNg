5a50d721e9dcbf1f90d807cbf347acacae32be38
Vittorio Adesso <adessovittorio8@gmail.com>
2022-08-20 12:05:11 +0200

    use pipe decimal

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -3,9 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-numeros',
   templateUrl: './numeros.component.html',
-  styleUrls: ['./numeros.component.css']
+  styleUrls: []
 })
 export class NumerosComponent implements OnInit {
+
+  ventasNeta: number = 26547388873.9430;
+  porcentaje: number = 0.48; 
 
   constructor() { }
 
