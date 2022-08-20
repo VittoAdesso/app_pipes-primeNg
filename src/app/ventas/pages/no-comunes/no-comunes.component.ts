32515836b1061673f5244bf3e435ef79a8520efc
Vittorio Adesso <adessovittorio8@gmail.com>
2022-08-20 16:39:15 +0200

    use pipe i18nSelect & plural with map

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -7,13 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoComunesComponent implements OnInit {
 
+  // i18nSelect
   name: string = 'Silvia'; 
   genero: string = 'femenino';
 
+  // mapa para trabajar, manejamos excepciones o posibles datos
   invitacionMapa= {
     'masculino': 'invitarlo',
     'femenino': 'invitarla'
   };
+
+  // i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Aquiles', 'José']; 
+
+  // mapa para trabajar, manejamos excepciones o posibles datos
+  clentesMapa ={
+    // si es igual a 0
+    '=0': 'No tenemos ningun cliente esperando.',
+    '=1': 'tenemos 1 cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    '=3': 'tenemos tres clientes esperando.',
+    // es lo mimso que un else el other y el # me cuenta el número de elmentos del array
+    'other': 'tenemos # clientes esperando.'
+  }
+
 
   constructor() { }
 
